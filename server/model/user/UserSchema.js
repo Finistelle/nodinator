b397c9f1ad0ba0ce9e0b0a3a489c39262a7ff45f
MAde <a.grandin42@gmail.com>
2017-08-29 13:43:27 +0200

    add sign-in path to register user

--- server/model/user/UserSchema.js
+++ server/model/user/UserSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 let UserSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  roles: String,
-  password: String,
-  email: String
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  roles: Array,
+  password: {type: String, required: true},
+  email: {type: String, required: true, unique: true}
 });
 
 let UserModel = mongoose.model('User', UserSchema);
--- server/routes/apiOauth.js
+++ server/routes/apiOauth.js
@@ -3,6 +3,58 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const config = require('./../../config');
 
+/* POST sign in user */
+router.post('/sign-in', (req, res) => {
+  let User = require("./../model/user/UserSchema");
+  user = new User();
+  if (null == req.body.firstName) {
+    return res.json({
+      "success": false,
+      "error": "malformed request missing argument firstName"
+    });
+  } else {
+    user.firstName = req.body.firstName;
+  }
+  if (null == req.body.lastName) {
+    return res.json({
+      "success": false,
+      "error": "malformed request missing argument lastName"
+    });
+  } else {
+    user.lastName = req.body.lastName;
+  }
+  if (null == req.body.password) {
+    return res.json({
+      "success": false,
+      "error": "malformed request missing argument password"
+    });
+  } else {
+    user.password = req.body.password;
+  }
+  if (null == req.body.email) {
+    return res.json({
+      "success": false,
+      "error": "malformed request missing argument email"
+    });
+  } else {
+    user.email = req.body.email;
+  }
+  user.roles.push('ROLE_USER');
+  try {
+    User.create(user);
+    res.json({
+      "success":true,
+      "user": user
+    });
+  } catch (e) {
+    res.json({
+      "success": false,
+      "error": e.message
+    })
+  }
+});
+
+/* POST authenticate user */
 router.post('/authenticate', (req, res) => {
   let User = require("./../model/user/UserSchema");
   User.findOne({
