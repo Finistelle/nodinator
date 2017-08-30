293aff243759b9391fb951b38266380d1580d0f0
Finistelle <s.touatimail@gmail.com>
2017-08-30 10:23:32 +0200

    rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,20 @@
-import { UserModule } from './user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RepositoryService } from "./commun/service/repository-service";
-import { ApiRepositoryService } from "./commun/service/api-repository-service";
-import { UserComponent } from "./user/component/user.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  // providers: [
-  //   {provide: RepositoryService, useClass: ApiRepositoryService}
-  // ],
+  providers: [],
   bootstrap: [AppComponent]
-
 })
 export class AppModule { }
--- src/app/user/model/user.model.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id: number;
-    firstname: string;
-    lastname: string;
-    role:Role
- 
-}
-
-export declare type Role = 'Administrateur' | 'Client' | 'Visiteur';
