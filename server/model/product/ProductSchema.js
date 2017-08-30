1c8961992dced729cb82af4c1c1489d44834ad87
MAde <a.grandin42@gmail.com>
2017-08-30 14:32:02 +0200

    update schema add timestamps and order and products schema

---
+++ server/model/product/ProductSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const URLSlugs = require('mongoose-url-slugs');
+
+let ProductSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    type: {type: String, required: true},
+    price: {type: Number, required: true},
+    media_id: {type: mongoose.Schema.ObjectId, ref: 'Media'},
+  },{
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+  }
+);
+
+ProductSchema.plugin(URLSlugs('title', {field: 'slug'}));
+
+let ProductModel = mongoose.Model('Product', ProductSchema);
+
+module.exports = ProductModel;
